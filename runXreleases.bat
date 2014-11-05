6767e90e1d85cd7f8da51a03148c8c9acd6e36e9
stkkj <stefan.krausekjaer@gmail.com>
2014-11-05 11:51:51 +0100

    Updated batch file to use the new project name

--- runXreleases.bat
+++ runXreleases.bat
@@ -5,13 +5,14 @@ set START_ID=%2
 
 IF "%START_ID%"=="" set START_ID=1
 
-cd HPPP_Controller
+cd DecentralizedParkPilot
 
 :Loop
 IF "%N_INSTANCES%"=="0" GOTO Continue
 	set /a id=%START_ID%+%N_INSTANCES%
 	echo Starting: %id%
-	start ..\x64\Release\HPPP_Controller.exe %id%
+	pwd
+	start ..\x64\Release\DecentralizedParkPilot.exe %id%
 
 	set /a N_INSTANCES=%N_INSTANCES%-1
 GOTO Loop
@@ -22,4 +23,4 @@ cd ..
 echo Press any key to kill all processes
 pause 
 
-taskkill /F /IM HPPP_Controller.exe /T
+taskkill /F /IM DecentralizedParkPilot.exe /T
