c2c81378be016325294def21c9fd732b65fe073a
Thomas Steffensen <tst@it-minds.dk>
2014-11-06 20:04:46 +0100

    piped output to nul

---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+
+cd DecentralizedParkPilot
+
+..\x64\Release\DecentralizedParkPilot.exe %id% > nul 2>&1
+
+exit
--- runXreleases.bat
+++ runXreleases.bat
@@ -5,19 +5,15 @@ set START_ID=%2
 
 IF "%START_ID%"=="" set START_ID=1
 
-cd DecentralizedParkPilot
-
 :Loop
 IF "%N_INSTANCES%"=="0" GOTO Continue
 	set /a id=%START_ID%+%N_INSTANCES%
 	echo Starting: %id%
-	start ..\x64\Release\DecentralizedParkPilot.exe %id%
+	start /B run.bat
 
 	set /a N_INSTANCES=%N_INSTANCES%-1
 GOTO Loop
 :Continue
-
-cd ..
 
 echo Press any key to kill all processes
 pause 
