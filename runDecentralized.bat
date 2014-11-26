3f97776cc423c76938f149ec36c8d8cae4a5c2bd
stkkj <stefan.krausekjaer@gmail.com>
2014-11-26 11:48:26 +0100

    Moved ID counter into the bat file actualy starting the client

--- runDecentralized.bat
+++ runDecentralized.bat
@@ -2,6 +2,8 @@
 
 cd DecentralizedParkPilot
 
+echo Starting: %id%
+
 ..\x64\Release\DecentralizedParkPilot.exe %id% > nul 2>&1
 
 exit
--- runXDecentralized.bat
+++ runXDecentralized.bat
@@ -3,18 +3,18 @@
 set N_INSTANCES=%1
 set START_ID=%2
 
-IF "%START_ID%"=="" set START_ID=1
+IF "%START_ID%"=="" set START_ID=0
 
 :Loop
 IF "%N_INSTANCES%"=="0" GOTO Continue
 	set /a id=%START_ID%+%N_INSTANCES%
-	echo Starting: %id%
 	start /B runDecentralized.bat
 
 	set /a N_INSTANCES=%N_INSTANCES%-1
 GOTO Loop
 :Continue
 
+sleep 2
 echo Press any key to kill all processes
 pause 
 
