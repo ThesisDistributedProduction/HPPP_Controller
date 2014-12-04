065b50b47279c83b297f63a942af809e2cc64773
Theis Nickelsen <theisnickelsen@gmail.com>
2014-12-04 19:17:18 +0100

    QoS. Removed topic QoS from centralized

--- ScriptsHelper/runCentralized.bat
+++ ScriptsHelper/runCentralized.bat
@@ -3,6 +3,6 @@ set number_of_turbines=%1
 
 cd ..\CentralizedParkPilot
 
-..\x64\Release\CentralizedParkPilot.exe -s -n %number_of_turbines%
+..\x64\Release\CentralizedParkPilot.exe -s -n %number_of_turbines% -msleep 0
 
 exit
