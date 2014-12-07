7436bdab6acc1a05447bc4f9672cb35f4db98023
Theis Nickelsen <theisnickelsen@gmail.com>
2014-12-07 12:42:15 +0100

    QoS for the centralized solution changes

--- ScriptsHelper/runCentralizedTurbine.bat
+++ ScriptsHelper/runCentralizedTurbine.bat
@@ -3,5 +3,5 @@
 cd ..\TurbineForCentralizedParkPilot
 
 REM ..\x64\Release\TurbineForCentralizedParkPilot.exe %id% > nul 2>&1
-..\x64\Release\TurbineForCentralizedParkPilot.exe -id %id% -s
+..\x64\Release\TurbineForCentralizedParkPilot.exe -id %id% -s -msleep 0
 exit
